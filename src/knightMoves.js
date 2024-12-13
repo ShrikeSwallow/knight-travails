33b0f04c74a2e40743b439fba5b0d930090bc1da
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-12-13 05:32:48 +0000

    Implement knightMoves using DFS

--- src/knightMoves.js
+++ src/knightMoves.js
@@ -1,35 +1,27 @@
 import { generateAdjList, convertCoordinates } from "./board";
 
-const board = generateAdjList();
-const search = (start, end) => {
+const search = (start, end, board, visited, path) => {
+  visited[start] = true;
+  path.push(start);
   if (start === end) {
     console.log("Success");
+    console.log(path.join(" -> "));
     return true;
   }
-  const visited = [];
-  visited.push(start);
-  const moves = [...board[start]];
-  console.log(moves);
-  let next;
-  /*while (moves.length > 0) {
-    next = moves.splice(0, 1)[0];
-    console.log(next);
-    if (!visited.includes(next)) {
-      search(next, end);
+  board[start].forEach((vertex) => {
+    if (visited[vertex] !== true) {
+      search(vertex, end, board, visited, path);
     }
-    visited.push(next);
-  }*/
-  console.log("search ended");
+  });
+  //console.log("search ended");
 };
 export const knightMoves = ([startX, startY], [endX, endY]) => {
   //console.log(board[start]);
   //console.log(board[start].includes(board[end]));
+  const board = generateAdjList();
   const start = convertCoordinates(startX, startY);
   const end = convertCoordinates(endX, endY);
-  console.log(start, end);
-  search(start, end);
+  const path = [];
+  const visited = [];
+  search(start, end, board, visited, path);
 };
-
-// MAKE A TREE FROM START POINT'S SORTED TABLE
-// ENSURE THERE'S NO REPETITIONS DURING INSERTION
-// USE CODE FROM BST PROJECT
